c8cd71f108c6aa5020f541dc97139628ed4f2a68
Patrick Azevedo de Menezes <patrick.menezes2001@gmail.com>
2021-06-10 23:42:48 -0400

    rename

--- ex 32.py
+++ ex 32.py
@@ -1,10 +1,10 @@
-line1 = float(input('Length of line 1: '))
-line2 = float(input('Length of line 2: '))
-line3 = float(input('Length of line 3: '))
+line1 = float(input('Length of segment 1: '))
+line2 = float(input('Length of segment 2: '))
+line3 = float(input('Length of segment 3: '))
 case1 = line3 < line1 + line2 
 case2 = line2 < line1 + line3
 case3 = line1 < line2 + line3
 if case1 and case2 and case3:
-    print('The three lines form a triangle')
+    print('The segments form a triangle')
 else:
-    print("The three lines don't form a triangle")
+    print("The segments don't form a triangle")

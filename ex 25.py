4b347548f2b0f2dd1d68c3f35bd461abddac0e76
PatrickAMenezes <patrick.menezes2001@gmail.com>
2021-12-08 19:19:01 -0400

    Update 1.1.0

---
+++ ex 25.py
@@ -0,0 +1,8 @@
+def write(phrase):
+    size = len(str(phrase)) + 4
+    print('-'*size)
+    print(' '*2 + phrase)
+    print('-'*size)
+
+phrase = str(input('Type a phrase: ')).strip()
+write(phrase)
---
+++ ex 28.py
@@ -0,0 +1,22 @@
+from random import randint
+numbers = []
+def drawn_numbers():
+    n = 0
+    while n < 5:
+        numbers.append(randint(0, 10))
+        n += 1
+
+def sum_even():
+    nums = 0
+    for n in range(len(numbers)):
+        if numbers[n] % 2 == 0:
+            nums += numbers[n]
+    return nums
+
+drawn_numbers()
+print('-'*55)
+print('Drawing 5 numbers: ', end='')
+for n in numbers:
+    print(n, end=' ')
+print(f'\nAdding the even values of {numbers}, we have {sum_even()}')
+print('-'*55)
